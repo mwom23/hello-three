7b109753c94fd7af9cabf665c9570dc3e46712ef
mwom23 <neutraljs@gmail.com>
2025-08-06 13:29:44 +0800

    #update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h2 v-for="(name,index) in names" :key="name">{{index}}{{ name }}</h2>
+<h2 v-for="name in fulNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
+<div v-for="actor in actors" :key="actor.name">
+  <h2>{{ actor.name }}</h2>
+  <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
+</div>
+<h2 v-for="(value, key, index) in myInfo" :key="value">{{ index }} {{ key }} {{ value }}</h2>
+
+<template v-for="name in names" :key="name">
+  <h2>{{ name }}</h2>
+  <hr />
+</template>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      names:['Bruce','Ammanda','Lee'],
+      fulNames:[
+        {first:'Bruce',last:'Chan'},
+        {first:'Ammanda',last:'Kim'},
+        {first:'Lee',last:'Soo'},
+      ],
+      actors:[
+        {name:'Adam',movies:['Love','Handle']},
+        {name:'Amelee',movies:['Sick','Man']},
+        {name:'Kee',movies:['Money','World']},
+      ],
+      myInfo: {
+        name: 'Dan',
+        channel:'Code',
+        course:'Vue 3',
+      }
+    }
+  },
 }
 </script>
 
